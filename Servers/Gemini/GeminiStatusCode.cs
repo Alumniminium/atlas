1ef628a47d80bcc79615f517381a303f4f7c3b94
trbl <domi@outlook.at>
2022-08-28 13:34:22 +0200

    refactor

--- Servers/Gemini/GeminiStatusCode.cs
+++ Servers/Gemini/GeminiStatusCode.cs
@@ -1,12 +1,5 @@
 namespace atlas
 {
-    public enum SpartanStatusCode
-    {
-        Success = 2,
-        Redirect = 3,
-        ClientError = 4,
-        ServerError = 5
-    }
     public enum GeminiStatusCode
     {
         Input = 10,
---
+++ Servers/Spartan/SpartanStatusCode.cs
@@ -0,0 +1,10 @@
+namespace atlas
+{
+    public enum SpartanStatusCode
+    {
+        Success = 2,
+        Redirect = 3,
+        ClientError = 4,
+        ServerError = 5
+    }
+}
